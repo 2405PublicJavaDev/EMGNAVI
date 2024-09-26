4e10393c3823643a744768fd0a6c32483d51fc9c
Absolute Value <heejun1708@gmail.com>
2024-09-26 16:10:51 +0900

    [Update] review vo 수정

---
+++ Back/emgnavi/src/main/java/com/emginfo/emgnavi/review/vo/Review.java
@@ -0,0 +1,21 @@
+package com.emginfo.emgnavi.review.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Review {
+
+  private String no;
+  private String writerId;
+  private String refNo;
+  private String content;
+  private Date createdDate;
+}
