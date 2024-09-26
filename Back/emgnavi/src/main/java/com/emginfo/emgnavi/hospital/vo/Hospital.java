66db7ba4f9d8777db781529ae36ce30865233813
Absolute Value <heejun1708@gmail.com>
2024-09-26 16:18:12 +0900

    [Add] Hospital vo 추가

---
+++ Back/emgnavi/src/main/java/com/emginfo/emgnavi/hospital/vo/Hospital.java
@@ -0,0 +1,70 @@
+package com.emginfo.emgnavi.hospital.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Hospital {
+
+  private String hpid;
+  private String dutyName;
+  private String dutyAddr;
+  private String dutyTel1;
+  private String erTel;
+  private String hvec;
+  private String hvoc;
+  private String hvcc;
+  private String hvncc;
+  private String hvccc;
+  private String hvicc;
+  private String hvgc;
+  private String dutyHayn;
+  private String dutyHano;
+  private String dutyinf;
+  private String dutyMaping;
+  private String dutyEryn;
+  private String dutyTime1C;
+  private String dutyTime2C;
+  private String dutyTime3C;
+  private String dutyTime4C;
+  private String dutyTime5C;
+  private String dutyTime6C;
+  private String dutyTime7C;
+  private String dutyTime8C;
+  private String dutyTime1S;
+  private String dutyTime2S;
+  private String dutyTime3S;
+  private String dutyTime4S;
+  private String dutyTime5S;
+  private String dutyTime6S;
+  private String dutyTime7S;
+  private String dutyTime8S;
+  private String mKioskTy25;
+  private String mKioskTy1;
+  private String mKioskTy2;
+  private String mKioskTy3;
+  private String mKioskTy4;
+  private String mKioskTy5;
+  private String mKioskTy6;
+  private String mKioskTy7;
+  private String mKioskTy8;
+  private String mKioskTy9;
+  private String mKioskTy10;
+  private String mKioskTy11;
+  private String wgs84Lon;
+  private String wgs84Lat;
+  private String dgidIdName;
+  private String hpbdn;
+  private String hpccuyn;
+  private String hpcuyn;
+  private String hperyn;
+  private String hpgryn;
+  private String hpicuyn;
+  private String hpnicfuyn;
+  private String hpopyn;
+}
