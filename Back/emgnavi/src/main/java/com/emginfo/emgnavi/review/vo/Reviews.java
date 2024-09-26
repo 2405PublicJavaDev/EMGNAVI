ad92d310be230dba881b72c0e4c541990598a92e
Absolute Value <heejun1708@gmail.com>
2024-09-26 16:41:16 +0900

    [Hotfix] 의존성 주입 이슈로 클래스 이름 수정

--- Back/emgnavi/src/main/java/com/emginfo/emgnavi/review/vo/Reviews.java
+++ Back/emgnavi/src/main/java/com/emginfo/emgnavi/review/vo/Reviews.java
@@ -5,17 +5,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.Date;
-
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
-public class Review {
+public class Reviews {
 
   private String no;
   private String writerId;
   private String refNo;
   private String content;
-  private Date createdDate;
+  private java.sql.Date createdDate;
 }
