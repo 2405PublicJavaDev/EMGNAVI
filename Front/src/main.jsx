0c20464453fa3bd7323dd81c7673c8a3d42dfd21
Absolute Value <heejun1708@gmail.com>
2024-09-30 10:29:47 +0900

    [Add] 페이지 이동시 스크롤이 맨 위로

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { UserProvider } from './UserContext';
+import { UserProvider } from './UserContext'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <UserProvider>
+      <ScrollToTop />
       <App />
     </UserProvider>
   </BrowserRouter>
