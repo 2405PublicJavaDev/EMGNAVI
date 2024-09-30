0c23119121b1646c4ccb833e26ef69c25b148402
Ella Eom <85346616+eomji1233@users.noreply.github.com>
2024-09-30 20:47:01 +0900

    Update App.jsx

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -19,6 +19,13 @@ import LoginMain from './pages/user/LoginMain';
 import FindEmail from './pages/user/FindEmail';
 import FIndEmailComplete from './pages/user/FindEmailComplete';
 import RegisterMain from './pages/user/registerMain';
+import FindPw from './pages/user/FindPw';
+import FindPwSendMSG from './pages/user/FindPwSendMSG';
+import ResetPw from './pages/user/ResetPw';
+import SetNickname from './pages/user/SetNickname';
+import MypageMain from './pages/user/Mypage';
+import MypageCheckPw from './pages/user/MypageCheckPw';
+import MypageModifyInf from './pages/user/MypageModifyInf';
 import PostNotice from './pages/notice/PostNotice';
 import GetNoticeList from './pages/notice/GetNoticeList';
 import GetNoticeDetail from './pages/notice/GetNoticeDetail';
@@ -58,10 +65,24 @@ function App() {
         <Route path='/user/register/verify' element={<RegisterVerify />} />
         <Route path='/user/register/page' element={<RegisterPage />} />
         <Route path='/user/register/complete' element={<RegisterComplete />} />
+
         <Route path='/user/login' element={<LoginMain />} />
+        <Route path="/user/setNickname" element={<SetNickname />} />
+
         <Route path='/user/findEmail' element={<FindEmail />} />
         <Route path='/user/findEmail/complete' element={<FIndEmailComplete />} />
 
+        <Route path='/user/findPw' element={<FindPw />} />
+        <Route path="/user/findPw/sent" element={<FindPwSendMSG />} />
+        <Route path="/user/findPw/resetPw" element={<ResetPw />} />
+
+        <Route path="/user/mypage" element={<MypageMain />} />
+        <Route path="/user/mypage/check" element={<MypageCheckPw />} />
+        <Route path="/user/mypage/modify" element={<MypageModifyInf />} />
+
+        <Route path='/notice/postNotice' element={<PostNotice />} />
+        <Route path='/notice/getNoticeList' element={<GetNoticeList />} />
+        <Route path='/notice/getNoticeDetail' element={<GetNoticeDetail />} />
         <Route path='/notice/postNotice' element={<PostNotice />} />
         <Route path='/notice/getNoticeList' element={<GetNoticeList />} />
         <Route path='/notice/getNoticeDetail' element={<GetNoticeDetail />} />
@@ -69,7 +90,6 @@ function App() {
         <Route path='*' element={<PageNotFound />} />
 
       </Routes>
-
     </>
   )
 }
