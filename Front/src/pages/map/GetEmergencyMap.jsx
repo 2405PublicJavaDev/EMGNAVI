4f63e49f323f0cc6065890c16634bfeb962f13c5
jwkim-portable <kh.jwkim@gmail.com>
2024-10-06 21:48:24 +0900

    [MOD] 검색 반경 조정 작업중 - 버튼 누르면 무한루프 발생하는 부분 고칠 예정

--- Front/src/pages/map/GetEmergencyMap.jsx
+++ Front/src/pages/map/GetEmergencyMap.jsx
@@ -9,8 +9,12 @@ function GetEmergencyMap() {
     const [hospitals, setHospitals] = useState([]);
     const [latitude, setLatitude] = useState(null);
     const [longitude, setLongitude] = useState(null);
-    const [map, setMap] = useState(null);
     const { response, error, loading, setConfig } = useAxios();
+    
+    const [map, setMap] = useState(null);
+    const [circle, setCircle] = useState(null);
+    const [zoomControl, setZoomControl] = useState(new kakao.maps.ZoomControl());
+    const [searchRadius, setSearchRadius] = useState(1000);
 
     const [positions, setPosition] = useState([]);
     const addMarkerPosition = (newPosition) => {
@@ -57,9 +61,40 @@ function GetEmergencyMap() {
             };
             setMap(new kakao.maps.Map(container, options));
 
+            // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+            // function zoomIn() {
+            //     map.setLevel(map.getLevel() - 1);
+            // }
+
+            // // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+            // function zoomOut() {
+            //     map.setLevel(map.getLevel() + 1);
+            // }
+
+            // if(map) {
+                // var zoomControl = new kakao.maps.ZoomControl();
+                // map.addControl(zoomControl, kakao.maps.ControlPosition.TOPRIGHT);
+            // }
+
+            // setCircle(
+            //     new kakao.maps.Circle({
+            //         center: new kakao.maps.LatLng(latitude, longitude), //원의 중심 좌표
+            //         radius: 50*map.getLevel(),                                         // 미터 단위의 원의 반지름
+            //         strokeWeight: 5,                                    // 선의 두께
+            //         strokeColor: '#75B8FA',                             // 선의 색깔
+            //         strokeOpacity: 1,                                   // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명
+            //         strokeStyle: 'dashed',                              // 선의 스타일
+            //         fillColor: '#CFE7FF',                               // 채우기 색깔
+            //         fillOpacity: 0.7                                    // 채우기 불투명도
+            //     })
+            // );
+            // // 설정한 원을 지도에 표시
+            // circle.setMap(map);
+            // console.log(50*map.getLevel());
+
             // 병원 데이터 가져오기
-            // fetch(`/api/map/getAroundEmgRoom?latitude=${latitude}&longitude=${longitude}&distance=10000`)
-            fetch(`http://127.0.0.1:8888/api/map/getAroundEmgRoom?latitude=${latitude}&longitude=${longitude}&distance=30000`)
+            fetch(`/api/map/getAroundEmgRoom?latitude=${latitude}&longitude=${longitude}&distance=10000`)
+            fetch(`http://127.0.0.1:8888/api/map/getAroundEmgRoom?latitude=${latitude}&longitude=${longitude}&distance=${searchRadius}`)
                 .then(response => response.json())
                 .then(data => {
                     setHospitals(data.data); // 받아온 병원 데이터를 State에 저장
@@ -68,7 +103,23 @@ function GetEmergencyMap() {
                     console.error('Error fetching hospital data:', error);
                 });
         }
-    }, [latitude, longitude])
+    }, [latitude, longitude]);
+
+    useEffect(() => {
+        if(searchRadius && latitude && longitude) {
+            if(hospitals){
+                setHospitals(null);
+            }
+            fetch(`http://127.0.0.1:8888/api/map/getAroundEmgRoom?latitude=${latitude}&longitude=${longitude}&distance=${searchRadius}`)
+                .then(response => response.json())
+                .then(data => {
+                    setHospitals(data.data); // 받아온 병원 데이터를 State에 저장
+                })
+                .catch(error => {
+                    console.error('Error fetching hospital data:', error);
+                });
+        }
+    }, [searchRadius, latitude, longitude]);
     
     useEffect(() => {
         if(map){
@@ -79,9 +130,12 @@ function GetEmergencyMap() {
                 map: map,
                 position: markerPosition,
             });
-        }
-    }, [map])
 
+            map.addControl(zoomControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+        }
+    }, [map]);
+    
     // useEffect(() => {
     //     if()
     // })
@@ -126,11 +180,102 @@ function GetEmergencyMap() {
         }
     }, [map, hospitals]);
 
+    // // 카카오맵의 level 변경을 감지하여 동작하는 useEffect
+    // useEffect(() => {
+    //     if (map) {
+    //         console.log(map.getLevel());
+
+    //         if (circle) {
+    //             circle.setMap(null);
+    //         }
+            
+    //         const newCircle = new kakao.maps.Circle({
+    //             center: new kakao.maps.LatLng(latitude, longitude), //원의 중심 좌표
+    //             radius: 50*map.getLevel(),                                         // 미터 단위의 원의 반지름
+    //             strokeWeight: 5,                                    // 선의 두께
+    //             strokeColor: '#75B8FA',                             // 선의 색깔
+    //             strokeOpacity: 1,                                   // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명
+    //             strokeStyle: 'dashed',                              // 선의 스타일
+    //             fillColor: '#CFE7FF',                               // 채우기 색깔
+    //             fillOpacity: 0.1                                    // 채우기 불투명도
+    //         });
+    //         setCircle(newCircle);
+    //         // 설정한 원을 지도에 표시
+    //         newCircle.setMap(map);
+    //         console.log(50*map.getLevel());
+    //     }
+    // }, [map?.getLevel()]);
+
+    // searchRadius 변경을 감지하여 동작하는 useEffect
+    useEffect(() => {
+        if (map) {
+            console.log(map.getLevel());
+
+            if (circle) {
+                circle.setMap(null);
+            }
+            
+            const newCircle = new kakao.maps.Circle({
+                center: new kakao.maps.LatLng(latitude, longitude), //원의 중심 좌표
+                radius: searchRadius,                                         // 미터 단위의 원의 반지름
+                strokeWeight: 5,                                    // 선의 두께
+                strokeColor: '#75B8FA',                             // 선의 색깔
+                strokeOpacity: 1,                                   // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명
+                strokeStyle: 'dashed',                              // 선의 스타일
+                fillColor: '#CFE7FF',                               // 채우기 색깔
+                fillOpacity: 0.1                                    // 채우기 불투명도
+            });
+            setCircle(newCircle);
+            // 설정한 원을 지도에 표시
+            newCircle.setMap(map);
+            console.log(searchRadius);
+        }
+    }, [searchRadius, latitude, longitude]);
+
+    // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+    function zoomIn() {
+        const newRadius = searchRadius + 1000;
+        
+        // 현재 searchRadius 값과 새로운 값이 다를 때만 업데이트
+        if (newRadius !== searchRadius) {
+            setSearchRadius(newRadius);
+    }
+    };
+
+    // // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 축소하는 함수입니다
+    // function zoomOut() {
+    //     map.setLevel(map.getLevel() + 1);
+    // };
+
     return (
-        <div id="map" style={{
-            width: '100%',
-            height: '900px'
-        }}></div>
+        <>
+            <div id="map" style={{
+                width: '100%',
+                height: '900px',
+                zIndex: 1
+            }}>
+            </div>
+            <div style={{
+                    position: 'absolute',
+                    top: '10px',
+                    right: '10px',
+                    overflow: 'hidden',
+                    width: '130px',
+                    height: '30px',
+                    margin: '0',
+                    padding: '0',
+                    zIndex: 100,
+                    fontSize: '12px',
+                    fontFamily: "'Malgun Gothic', '맑은 고딕', sans-serif",
+                    border: '1px solid #919191',
+                    borderRadius: '5px'
+                }}>
+                <button onClick={() => zoomIn()} >
+                ZoomIn
+                </button>
+
+            </div>
+        </>
     )
 }
 
