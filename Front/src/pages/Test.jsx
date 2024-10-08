e1495b52c882292c8b7b91e2cbf033d3ba3f2966
AbsoluteValues <heejun1708@gmail.com>
2024-10-09 03:52:35 +0900

    [Update] useAxios 커스텀 훅 재설계 완료

--- Front/src/pages/Test.jsx
+++ Front/src/pages/Test.jsx
@@ -6,63 +6,45 @@ const Test = () => {
 
     const { no } = useParams();
 
-    // 요청을 하나만 진행할 때
-    const { response, error, loading, fetchData } = useAxios({
-        method: 'GET',
-        url: `/api/test/${no}`,
-    });
-
-    // 두 가지 요청이 필요할 때(update)
-    const { response: response1, error: error1, loading: loading1, fetchData: fetchData1 } = useAxios({
-        method: 'GET',
-        url: `/api/test/${no}`,
-    });
-    const { response: response2, error: error2, loading: loading2, fetchData: fetchData2 } = useAxios({
-        method: 'POST',
-        url: `/api/test`,
-        data: {
-            data: '1'
-        }
-    });
+    const { response, fetchData } = useAxios();
 
     useEffect(() => {
-        fetchData1();
+        fetchData(
+            {
+                method: 'GET',
+                url: `/api/test/${no}`,
+            },
+        );
     }, []);
 
     const button = async () => {
-        const test = await fetchData();
-        console.log(test);
-
-        if (test == 200) {
-            console.log("다음 동작 시작!");
-        }
-        // fetchData2();
-
-        // console.log(response2.data);
+        fetchData(
+            {
+                method: 'POST',
+                url: `/api/test/2`,
+                data: {
+                    no,
+                }
+            },
+            (data) => {
+                console.log(data);
+                if (data.status === 200) {
+                    console.log("다음 동작!!!");
+                }
+            }
+        );
     }
 
     return (
         <>
-            <div className="flex items-center justify-center h-screen bg-gray-100">
-                {loading1 && (
-                    <p>Loading...</p>
-                )}
-                {error1 && (
-                    <>
-                        <p>{error1.status}</p>
-                        <p>{error1.error}</p>
-                        <p>{error1.message}</p>
-                        <p>에러 코드 : {error1.code}</p>
-                    </>
-                )}
-                {!loading1 && !error1 && (
+            {response && (
+                <div className="flex items-center justify-center h-screen bg-gray-100">
                     <div className="text-center">
-                        <h1 className="text-6xl font-extrabold text-black mb-4">{response1.data}</h1>
+                        <h1 className="text-6xl font-extrabold text-black mb-4">{response.data}</h1>
                     </div>
-
-                )}
-                <button onClick={button}>Test</button>
-            </div >
+                    <button onClick={button}>Test</button>
+                </div >
+            )}
         </>
     )
 }
