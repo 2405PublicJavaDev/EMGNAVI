93a2b3ca80b28fa0ebe004d7a6e09b7ace3a3ee3
jwkim-portable <kh.jwkim@gmail.com>
2024-10-09 06:51:57 +0900

    [ADD] 통계 그래프 작성을 위한 백엔드 구조 작성
    - 기본적인 클래스, 메서드 형태만 만들어놓은 상태
    - 내용 작성은 10/[13-14] 에 진행할 예정

---
+++ Back/emgnavi/src/main/java/com/emginfo/emgnavi/stat/mapper/StatMapper.java
@@ -0,0 +1,7 @@
+package com.emginfo.emgnavi.stat.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StatMapper {
+}
---
+++ Back/emgnavi/src/main/java/com/emginfo/emgnavi/stat/service/StatService.java
@@ -0,0 +1,15 @@
+package com.emginfo.emgnavi.stat.service;
+
+import com.emginfo.emgnavi.stat.vo.Stat;
+
+import java.util.List;
+
+public interface StatService {
+
+    /**
+     * 통계의 종류(요일별, 월별, 요일 및 시간별 등등...)를 입력받아 그래프를 그리기 위한 포맷으로 리턴하는 서비스
+     * @param statType
+     * @return List<Stat>
+     */
+    List<Stat> getStatInfo(String statType);
+}
