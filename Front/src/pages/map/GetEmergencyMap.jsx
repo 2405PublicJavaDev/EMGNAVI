860d857ce25b9dc3ca2f96fdb7bdcf869b703126
kh-jwkim <kh.jwkim@gmail.com>
2024-10-17 10:30:03 +0900

    [MOD] 응급실 지도 페이지에서 사이드바에 페이지 이동 할 수 있게 설정

--- Front/src/pages/map/GetEmergencyMap.jsx
+++ Front/src/pages/map/GetEmergencyMap.jsx
@@ -265,12 +265,12 @@ function GetEmergencyMap() {
                 <div className="flex w-[25%] h-[100vh] bg-white p-4">
                     <div className="flex flex-col w-[25%]">
                         <h1>
-                            <img className="left-[24px] top-0" width="111" height="97" src="/img/header/logo.png" alt="Logo"></img>
+                            <img className="left-[24px] top-0 cursor-pointer" width="111" height="97" src="/img/header/logo.png" alt="Logo" onClick={() => window.location.href = '/'}></img>
                         </h1>
                         <div className="text-center font-bold bg-[#0B2D85] text-[#ffffff] py-3"><button>응급실</button></div>
-                        <div className="text-center font-bold py-3"><button>병원</button></div>
-                        <div className="text-center font-bold py-3"><button>약국</button></div>
-                        <div className="text-center font-bold py-3"><button>AED</button></div>
+                        <div className="text-center font-bold py-3"><button onClick={() => window.location.href = 'getHospitalMap'}>병원</button></div>
+                        <div className="text-center font-bold py-3"><button onClick={() => window.location.href = 'getPharmacyMap'}>약국</button></div>
+                        <div className="text-center font-bold py-3"><button onClick={() => window.location.href = 'getAedMap'}>AED</button></div>
                     </div>
                     <div className="flex flex-col w-[75%]">
                         <div className="space-y-3 px-[10px]">
