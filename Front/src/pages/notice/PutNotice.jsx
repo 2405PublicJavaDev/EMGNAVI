3d86ed5e69a2144f33531504da4cdcc6bbd32eab
kh-jwkim <kh.jwkim@gmail.com>
2024-10-18 14:09:37 +0900

    [FIX] 공지 관련 페이지 footer 이미지 경로 변경

--- Front/src/pages/notice/PutNotice.jsx
+++ Front/src/pages/notice/PutNotice.jsx
@@ -50,16 +50,16 @@ const PutNotice = () => {
             <div className="absolute left-0 top-[1189px] w-[100%] h-[232px] bg-[#000] overflow-hidden">
                 <div className="absolute left-[136px] top-[41px] w-[117px] h-[126px] flex">
                     <div className="absolute left-[13px] top-[97px] text-[24px] font-['Advent_Pro'] font-black text-[#333] whitespace-nowrap">응급NAVI</div>
-                    <img className="absolute left-0 top-0" width="117" height="100" src="/img/user/logo.png"></img>
+                    <img className="absolute left-0 top-0" width="117" height="100" src="/img/footer/logo.png"></img>
                 </div>
                 <div className="absolute left-[390px] top-[62px] w-[638px] h-[115px] flex">
                     <div className="absolute left-0 top-[54px] w-[638px] h-[61px] flex">
-                        <div className="absolute left-0 top-0 w-[638px] h-[61px] text-[14px] leading-[150%] font-['Agdasima'] font-bold text-[#686868]">서울 중구 남대문로 120 대일빌딩 2층, 3층 KH정보교육원 종로지원     |     대표자명 : 민봉식     |     대표전화 : 1544-9970<br />     2024 응급NAVI.<br /></div>
-                        <img className="absolute left-[2px] top-[27px]" width="9" height="8" src="/img/user/copyright (1) 1150_102.png"></img>
+                        <div className="absolute left-0 top-0 w-[638px] h-[61px] text-[14px] leading-[150%] font-['Agdasima'] font-bold text-[#686868]">서울 중구 남대문로 120 대일빌딩 2층, 3층 KH정보교육원 종로지원     |     대표자명 : 민봉식     |     대표전화 : 1544-9970<br />&nbsp;&nbsp;&nbsp;&nbsp; 2024 응급NAVI.<br /></div>
+                        <img className="absolute left-[2px] top-[27px]" width="9" height="8" src="/img/footer/copyright.png"></img>
                     </div>
                     <div className="absolute left-0 top-0 w-[221px] h-[21px] text-[15px] leading-[150%] font-['Agdasima'] font-bold text-[#686868]">이용약관              개인정보처리방침</div>
                 </div>
-                <img className="absolute left-[1634px] top-[47px]" width="145" height="34" src="/img/user/Group 17150_104.png"></img>
+                <img className="absolute left-[1634px] top-[47px]" width="145" height="34" src="/img/footer/group.png"></img>
             </div>
 
             <div className="absolute left-0 top-[124px] w-[100%] h-[194px] overflow-hidden">
