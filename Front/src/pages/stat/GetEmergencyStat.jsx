3c3b04f34ee86ce60a38ee5b126a97ef892d847c
jwkim-portable <kh.jwkim@gmail.com>
2024-10-21 10:28:56 +0900

    [MOD] 통계페이지 footer에서 absolute 제거

--- Front/src/pages/stat/GetEmergencyStat.jsx
+++ Front/src/pages/stat/GetEmergencyStat.jsx
@@ -294,21 +294,32 @@ const GetEmergencyStat = () => {
                 </div>
             </main>
 
-            <div className="absolute left-0 top-[1900px] w-[1920px] h-[232px] bg-[#000] overflow-hidden">
-                <div className="absolute left-[136px] top-[41px] w-[117px] h-[126px] flex">
-                    <div className="absolute left-[13px] top-[97px] text-[24px] font-['Advent_Pro'] font-black text-[#333] whitespace-nowrap">응급NAVI</div>
-                    <img className="absolute left-0 top-0" width="117" height="100" src="/img/footer/logo.png"></img>
-                </div>
-                <img className="absolute left-[1634px] top-[47px]" width="145" height="34" src="/img/footer/group.png"></img>
-                <div className="absolute left-[404px] top-[137px] w-[621px] h-[16px] text-[14px] leading-[150%] font-['Agdasima'] font-bold text-[#686868]">2024 응급NAVI.</div>
-                <div className="absolute left-[390px] top-[62px] w-[742px] h-[90px] flex">
-                    <div className="absolute left-0 top-[54px] w-[742px] h-[36px] flex">
-                        <div className="absolute left-0 top-0 w-[742px] h-[16px] text-[14px] leading-[150%] font-['Agdasima'] font-bold text-[#686868]">서울 중구 남대문로 120 대일빌딩 2층, 3층 KH정보교육원 종로지원     |     대표자명 : 민봉식     |     대표전화 : 1544-997<br /></div>
-                        <img className="absolute left-[2px] top-[27px]" width="9" height="8" src="/img/footer/copyright.png"></img>
+            <div className="w-full mt-[100px] bg-black py-10">
+                    <div className="container mx-auto flex flex-col md:flex-row items-start justify-between">
+                        <div className="mb-8 md:mb-0">
+                            <img className="w-[117px] h-[100px]" src="/img/footer/logo.png" alt="Logo" />
+                            <div className="mt-2 text-2xl font-black text-[#333] font-['Advent_Pro']">응급NAVI</div>
+                        </div>
+
+                        <div className="flex flex-col max-w-[638px]">
+                            <div className="mb-4 text-sm font-bold text-[#686868] font-['Agdasima']">
+                                이용약관              개인정보처리방침
+                            </div>
+                            <div className="text-sm leading-relaxed font-bold text-[#686868] font-['Agdasima']">
+                                서울 중구 남대문로 120 대일빌딩 2층, 3층 KH정보교육원 종로지원     |     대표자명 : 민봉식     |     대표전화 : 1544-9970
+                                <br />
+                                <span className="flex items-center">
+                                    <img className="w-2 h-2 mr-1" src="/img/footer/copyright.png" alt="Copyright" />
+                                    2024 응급NAVI.
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="mt-8 md:mt-0">
+                            <img className="w-[145px] h-[34px]" src="/img/footer/group.png" alt="Group" />
+                        </div>
                     </div>
-                    <div className="absolute left-0 top-0 w-[221px] h-[21px] text-[15px] leading-[150%] font-['Agdasima'] font-bold text-[#686868]">이용약관              개인정보처리방침</div>
                 </div>
-            </div>
         </div>
     );
 }
