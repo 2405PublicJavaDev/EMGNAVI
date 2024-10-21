ada5d06e31304521f66595caec460be9535986bb
mangyuroykim <qweasd1599@naver.com>
2024-10-21 16:50:45 +0900

    헤더 로고 눌리게 설정
    z인덱스로 레이어 구분하고 커서포인터 별도 지정후 해결

--- Front/src/pages/Header.jsx
+++ Front/src/pages/Header.jsx
@@ -23,8 +23,8 @@ const Header = () => {
         if (isConfirmed) {
             await axios.post('/api/logout');
             handleReload(true);
-            nav("/user/login"); // 로그아웃 후 로그인 페이지로 이동
-        } // 취소 시 아무 동작도 하지 않음
+            nav("/user/login"); 
+        } 
     };
     const handlerGoMypage = () => {
         nav("/user/mypage");
@@ -36,7 +36,7 @@ const Header = () => {
     return (
         <>
             {(!location.pathname.startsWith("/map")) &&
-                <div className="absolute left-0 top-0 w-[100%] h-[161px] overflow-hidden z-10">
+                <div className="absolute left-0 top-0 w-[100%] h-[161px] overflow-hidden">
                     <div className="absolute left-0 top-0 w-[100%] h-[155px] flex">
                         <div className="absolute left-0 top-[31px] w-[100%] h-[93px] flex">
                             <div className="absolute left-0 top-0 w-[100%] h-[93px] bg-[#fff]"></div>
@@ -90,18 +90,19 @@ const Header = () => {
                                 </div>
                             )}
                         </div>
-                        <button onClick={handlerGoMain} className="absolute left-[857px] top-0 w-[207px] h-[155px] flex z-99">
+                        <button 
+                            onClick={handlerGoMain} 
+                            className="absolute left-[857px] top-0 w-[207px] h-[155px] flex z-50 cursor-pointer"
+                        >
                             <img className="absolute left-0 right-0 top-0 bottom-0" width="206" height="154" src="/img/header/background.png"></img>
                             <img className="absolute left-[2.21%] right-[2.8%] top-0 bottom-[2.63%]" width="196" height="150" src="/img/header/line.png"></img>
-                            <div
-                                style={{ cursor: 'pointer' }}
-                                className="absolute left-[24px] top-[6px] w-[159px] h-[123px] flex">
+                            <div className="absolute left-[24px] top-[6px] w-[159px] h-[123px] flex">
                                 <div className="absolute left-0 top-[95px] w-[159px] h-[28px] text-[24px] font-['Advent_Pro'] font-black text-[#0b2d85] text-center">응급NAVI</div>
                                 <img className="absolute left-[24px] top-0" width="111" height="97" src="/img/header/logo.png"></img>
                             </div>
                         </button>
                     </div>
-                    <div className="absolute left-[264px] top-[60px] w-[1393px] h-[23px] flex z-1">
+                    <div className="absolute left-[264px] top-[60px] w-[1393px] h-[23px] flex z-40">
                         <button className="absolute left-0 top-0 text-[16px] font-['Jost'] font-bold text-[#000] whitespace-nowrap hover:border-b-2 hover:border-gray-600 hover:text-gray-700 hover:transition-all hover:duration-300" onClick={() => nav('/map/getEmergencyMap')}>내비게이션</button>
                         <button className="absolute left-[208px] top-0 text-[16px] font-['Jost'] font-bold text-[#000] whitespace-nowrap hover:border-b-2 hover:border-gray-600 hover:text-gray-700 hover:transition-all hover:duration-300" onClick={() => nav('/hospital/HospitalSearch')}>병원검색</button>
                         <button className="absolute left-[401px] top-0 text-[16px] font-['Jost'] font-bold text-[#000] whitespace-nowrap hover:border-b-2 hover:border-gray-600 hover:text-gray-700 hover:transition-all hover:duration-300" onClick={() => nav('/pharmacy/PharmacySearch')}>약국검색</button>
@@ -109,9 +110,7 @@ const Header = () => {
                         <button className="absolute left-[1093px] top-0 text-[16px] font-['Jost'] font-bold text-[#000] whitespace-nowrap hover:border-b-2 hover:border-gray-600 hover:text-gray-700 hover:transition-all hover:duration-300" onClick={() => nav('/stat/getEmergencyStat')}>응급실 통계</button>
                         <button className="absolute left-[1329px] top-0 text-[16px] font-['Jost'] font-bold text-[#000] whitespace-nowrap hover:border-b-2 hover:border-gray-600 hover:text-gray-700 hover:transition-all hover:duration-300" onClick={() => nav('/notice/getNoticeList')}> 공지사항</button>
                     </div>
-
                 </div>
-
             }
         </>
     )
